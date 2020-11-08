78c44dfc1d450dce402f69d3387e82be7e69ec71
NickWanchai <wanchai93@hotmail.dk>
2020-11-08 13:05:37 +0100

    hu

--- src/test/java/webshop/webshop/WebshopApplicationTests.java
+++ src/test/java/webshop/webshop/WebshopApplicationTests.java
@@ -3,11 +3,18 @@ package webshop.webshop;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class WebshopApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void add() {
+        assertEquals(15,10+5);
     }
 
 }
